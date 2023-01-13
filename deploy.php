74497ee3a523287ef12744ad9d154af872f58b7c
jiannei <longjian.huang@foxmail.com>
2023-01-13 15:50:55 +0800

    deploy

---
+++ deploy.php
@@ -0,0 +1,30 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Config
+
+set('repository', 'https://github.com/jiannei/jarvis.git');
+
+add('shared_files', [
+    'database/database.sqlite'
+]);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+host('jarvis.coderplanets.cn')
+    ->setRemoteUser('deployer')
+    ->setPort(22)
+    ->setidentityFile('~/.ssh/deploykey')
+    ->setForwardAgent(true)
+    ->setSshMultiplexing(true)
+    ->setSshArguments(['-o UserKnownHostsFile=/dev/null'])
+    ->set('http_user', 'www')
+    ->set('branch', 'main')
+    ->set('deploy_path', '/www/wwwroot/jarvis');
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
