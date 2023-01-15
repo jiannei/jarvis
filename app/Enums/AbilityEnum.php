53b1d92c3716de8f3688c3abb072bb4cf6052a14
jiannei <longjian.huang@foxmail.com>
2023-01-15 15:43:25 +0800

    ability enum

---
+++ app/Enums/AbilityEnum.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enums;
+
+use Jiannei\Enum\Laravel\Enum;
+
+class AbilityEnum extends Enum
+{
+    public const SERVICE_API = 'service:api';
+}
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\AbilityEnum;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
 
@@ -12,7 +13,7 @@ class HomeController extends Controller
         $token = null;
         if (Auth::check()) {
             $token = Cache::remember('auth:user:'.Auth::id(), now()->addSeconds(config('sanctum.expiration')), function () {
-                return Auth::user()->createToken(config('app.name'),['service:api'])->plainTextToken;
+                return Auth::user()->createToken(config('app.name'),[AbilityEnum::SERVICE_API])->plainTextToken;
             });
         }
 
