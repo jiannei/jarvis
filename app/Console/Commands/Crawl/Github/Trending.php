21bd65ddb87a8d64121073c3f90f4adbb4da290c
jiannei <longjian.huang@foxmail.com>
2023-01-16 16:32:16 +0800

    fix github trending

--- app/Console/Commands/Crawl/Github/Trending.php
+++ app/Console/Commands/Crawl/Github/Trending.php
@@ -41,7 +41,7 @@ class Trending extends Command
             $bar->start();
 
             foreach ($this->params() as $param) {
-                [$language, $spokenLanguageCode, $since] = array_values($param);
+                [$language, $spokenLanguageCode] = array_values($param);
 
                 CrawlGithubTrending::dispatch($language, $spokenLanguageCode, $since);
 
@@ -60,11 +60,11 @@ class Trending extends Command
     private function params(): array
     {
         return [
-            ['language' => null, 'spoken_language_code' => 'zh', 'since' => 'daily'],
-            ['language' => 'php', 'spoken_language_code' => 'zh', 'since' => 'daily'],
-            ['language' => 'golang', 'spoken_language_code' => 'zh', 'since' => 'daily'],
-            ['language' => 'javascript', 'spoken_language_code' => 'zh', 'since' => 'daily'],
-            ['language' => 'vue', 'spoken_language_code' => 'zh', 'since' => 'daily'],
+            ['language' => null, 'spoken_language_code' => 'zh'],
+            ['language' => 'php', 'spoken_language_code' => 'zh'],
+            ['language' => 'golang', 'spoken_language_code' => 'zh'],
+            ['language' => 'javascript', 'spoken_language_code' => 'zh'],
+            ['language' => 'vue', 'spoken_language_code' => 'zh'],
         ];
     }
 }
