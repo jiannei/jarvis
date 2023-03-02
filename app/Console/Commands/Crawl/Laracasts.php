d773fde34fae3e647ae49c2190bb688842c2a872
jiannei <longjian.huang@foxmail.com>
2023-03-02 15:26:30 +0800

    wip: chrome driver

---
+++ app/Console/Commands/Crawl/Laracasts.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Console\Commands\Crawl;
+
+use App\Services\CrawlerService;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\WebDriverBy;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Auth;
+use Jiannei\LaravelCrawler\Support\Facades\Crawler;
+use Spatie\Browsershot\Browsershot;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+
+class Laracasts extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'crawl:laracasts {menu}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '更新「laracasts」';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(CrawlerService $service): void
+    {
+        $this->info("[{$this->description}]:执行开始 ".now()->format('Y-m-d H:i:s'));
+
+        Auth::onceUsingId(1);
+
+        $serverUrl = 'http://localhost:4444';
+        $driver = RemoteWebDriver::create($serverUrl, DesiredCapabilities::chrome());
+        $driver->get('https://laracasts.com/series');
+
+        $driver->wait()->until(\Facebook\WebDriver\WebDriverExpectedCondition::titleIs('Laracasts Series'));
+
+        $element = $driver->findElement(
+            WebDriverBy::cssSelector('body')
+        );
+
+        $title = $element->getDomProperty('innerHTML');
+
+        $driver->quit();
+
+        $crawler = Crawler::new($title);
+
+        $header = $crawler->parse([
+            'title' => ['header h3','text'],
+        ]);
+
+        dd($header);
+
+
+        $posts = $service->handleLaracasts($this->argument('menu'));
+
+
+        $this->info("[{$this->description}]:执行结束 ".now()->format('Y-m-d H:i:s'));
+    }
+}
