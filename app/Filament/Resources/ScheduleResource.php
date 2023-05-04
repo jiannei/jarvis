c57ed35cef2fe839b95065a383a1a61b27422d25
jiannei <longjian.huang@foxmail.com>
2023-05-04 17:06:09 +0800

    feat: schedule

---
+++ app/Filament/Resources/ScheduleResource.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ScheduleResource\Pages;
+use App\Filament\Resources\ScheduleResource\RelationManagers;
+use App\Models\Schedule;
+use Cron\CronExpression;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Filament\Tables\Columns\IconColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Carbon;
+
+class ScheduleResource extends Resource
+{
+    protected static ?string $model = Schedule::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('id')->sortable(),
+                Tables\Columns\TextColumn::make('description'),
+                Tables\Columns\TextColumn::make('command'),
+                Tables\Columns\TextColumn::make('parameters'),
+                Tables\Columns\TextColumn::make('expression'),
+                Tables\Columns\TextColumn::make('next')->getStateUsing(function (Schedule $record) {// todo 使用 pref 关联关系
+                    return self::nextDueDate($record);
+                }),
+                Tables\Columns\IconColumn::make('active')->boolean(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('updated_at')->sortable()
+                    ->dateTime(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSchedules::route('/'),
+//            'create' => Pages\CreateSchedule::route('/create'),
+//            'edit' => Pages\EditSchedule::route('/{record}/edit'),
+        ];
+    }
+
+    private static function nextDueDate($schedule)
+    {
+        return Carbon::instance((new CronExpression($schedule->expression))
+            ->getNextRunDate(Carbon::now()->setTimezone($schedule->timezone))
+            ->setTimezone(new \DateTimeZone($schedule->timezone))
+        )->diffForHumans();
+    }
+}
---
+++ app/Models/Schedule.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Schedule extends \Jiannei\Schedule\Laravel\Repositories\Models\Schedule
+{
+    use HasFactory;
+}
