e518ff44a9dac029ee3f9f23294a30c154d4084a
jiannei <longjian.huang@foxmail.com>
2023-05-10 15:26:49 +0800

    fix: feeds cache key

--- app/Http/Controllers/Api/JarvisController.php
+++ app/Http/Controllers/Api/JarvisController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Jobs\ActivityLog;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Cache;
 use Jiannei\LaravelCrawler\Support\Facades\Crawler;
 use Jiannei\Response\Laravel\Support\Facades\Response;
@@ -13,13 +14,15 @@ class JarvisController extends Controller
 {
     public function feeds(Request $request, string $source)
     {
-        $result = Cache::remember('feeds:'.$source, now()->addMinute(), function () use ($source, $request) {
-            try {
-                return Crawler::json($source, $request->all());
-            } catch (\Throwable $e) {
-                abort(404, $e->getMessage());
-            }
-        });
+        $result = Cache::remember('feeds:'.md5($source.':'.Arr::join($request->all(), ':')),
+            now()->addMinute(),
+            function () use ($source, $request) {
+                try {
+                    return Crawler::json($source, $request->all());
+                } catch (\Throwable $e) {
+                    abort(404, $e->getMessage());
+                }
+            });
 
         dispatch(new ActivityLog($source, $request->user(), ['ip' => $request->ip()], 'crawler'));
 
